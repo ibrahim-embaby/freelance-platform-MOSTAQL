32331a2191ffb4d9c9c8d144e2141a08c20379fc
ibrahim-embaby <ibrahimembaby10@gmail.com>
2022-12-16 19:47:57 +0200

    changes to profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import Footer from '../../components/footer/Footer'
 import Topbar from '../../components/topbar/Topbar'
+import { FaUser, FaMapMarkerAlt } from 'react-icons/fa'
+import { IoBriefcaseSharp } from 'react-icons/io5'
+import { BiSliderAlt } from 'react-icons/bi'
 import './profile.css'
 
 
@@ -12,6 +15,35 @@ function Profile() {
     return (
         <div className='profile'>
             <Topbar />
+            <div className="personalInfo">
+                <div className="personalInfoTop">
+                    <img className="personalInfoTopImg" src="assets/user.png" alt="" />
+                    <div className="personalInfoTopUsername">Ibrahim Embaby</div>
+                    <div className="personalInfoTopDetails">
+                        <span className='personalInfoTopDetailsIconContainer'><FaUser className='personalInfoTopDetailsIcon' /> مستقل</span>
+                        <span className='personalInfoTopDetailsIconContainer'><IoBriefcaseSharp className='personalInfoTopDetailsIcon' />  برمجة، تطوير المواقع والتطبيقات</span>
+                        <span className='personalInfoTopDetailsIconContainer'><FaMapMarkerAlt className='personalInfoTopDetailsIcon' /> مصر</span>
+                    </div>
+                </div>
+                <div className="personalInfoBottom">
+                    <div className="personalInfoBottomLeft">
+                        <div className="personalInfoBottomLeftItem active">
+                            <FaUser className='personalInfoBottomLeftItemIcon' /> الملف الشخصي
+                        </div>
+                        <div className="personalInfoBottomLeftItem">
+                            <IoBriefcaseSharp className='personalInfoBottomLeftItemIcon' /> معرض الأعمال
+                        </div>
+                    </div>
+                    <div className="personalInfoBottomRight">
+                        <BiSliderAlt className='personalInfoBottomRightIcon' />
+                        تعديل الملف الشخصي
+                    </div>
+                </div>
+            </div>
+            <div className="profileWrapper">
+                <div className="profileWrapperRight"></div>
+                <div className="profileWrapperLeft"></div>
+            </div>
             <Footer />
         </div>
     )
